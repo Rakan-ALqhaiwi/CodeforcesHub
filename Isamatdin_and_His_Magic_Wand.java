deea77522973fe7b3271a26aec6ab20f09f49bd3
Rakan AL-qhiwi <alqhiwirakan@gmail.com>
2025-11-17 21:02:05 +0300

    Add files via upload

---
+++ Isamatdin_and_His_Magic_Wand.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.isamatdin_and_his_magic_wand;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class Isamatdin_and_His_Magic_Wand {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt();
+            int x[] = new int[n];
+            for (int j = 0; j < x.length; j++) {
+              x[j]=in.nextInt();
+
+            }
+            for (int j = 0; j < x.length; j++) {
+                if (x[j] % 2 ==0) {
+                    
+                }
+                else 
+                    
+            }
+        }
+      
+    }
+}
